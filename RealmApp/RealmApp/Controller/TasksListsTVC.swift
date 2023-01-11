52ba8ebea19a198213b82cf755a8b557779079fe
Dmitry Satskevich <Satsckevich1993@yandex.ru>
2023-01-11 21:07:39 +0300

    the first part is done

--- RealmApp/RealmApp/Controller/TasksListsTVC.swift
+++ RealmApp/RealmApp/Controller/TasksListsTVC.swift
@@ -8,89 +8,139 @@
 import UIKit
 import RealmSwift
 
-class TasksListsTVC: UITableViewController {
+final class TasksListsTVC: UITableViewController {
     
     var tasksLists: Results<TasksList>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // выгрузка из БД + сортировка
+        tasksLists = StorageManager.getAllTasksLists().sorted(byKeyPath: "name")
 
         let add = UIBarButtonItem(barButtonSystemItem: .add,
                                   target: self,
                                   action: #selector(addBarButtonSystemItemSelector))
         self.navigationItem.setRightBarButtonItems([add, editButtonItem], animated: true)
-        
     }
-
+    
+    
+    @IBAction func segmentedControlChanget(_ sender: UISegmentedControl) {
+        let byKeyPath = sender.selectedSegmentIndex == 0 ? "name" : "date"
+        tasksLists = tasksLists.sorted(byKeyPath: byKeyPath)
+        tableView.reloadData()
+    }
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        tasksLists.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        // Configure the cell...
-
+        let taskList = tasksLists[indexPath.row]
+        cell.textLabel?.text = taskList.name
+        cell.detailTextLabel?.text = taskList.tasks.count.description
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
+    // Свайп ячейки
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        // Вытащить текущий лист
+        let currentList = tasksLists[indexPath.row]
+
+        // Создание кнопок в ячейке
+        let deleteContextItem = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
+            StorageManager.deleteList(currentList)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+
+        let editeContextItem = UIContextualAction(style: .destructive, title: "Edite") { _, _, _ in
+            self.alertForAddAndUpdatesListTasks(currentList) {
+                tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+        }
+
+        let doneContextItem = UIContextualAction(style: .destructive, title: "Done") { _, _, _ in
+//            StorageManager.makeAllDone(currentList)
+//            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+
+        editeContextItem.backgroundColor = .orange
+        doneContextItem.backgroundColor = .green
+
+        let swipeAtions = UISwipeActionsConfiguration(actions: [deleteContextItem, editeContextItem, doneContextItem])
+
+        return swipeAtions
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    @objc func addBarButtonSystemItemSelector() {
         
     }
+    
+    @objc func addBarButtonSystemItemSelector() {
+        alertForAddAndUpdatesListTasks { [weak self] in
+            print("List added or edited")
+            self?.tableView.reloadData()
+        }
+    }
+    
+    // Делаем alertForAddAndUpdatesListTasks универсальной функцией
+    private func alertForAddAndUpdatesListTasks(_ tasksList: TasksList? = nil,
+                                                complition: @escaping () -> Void)
+    {
+        // Заголовок Alerta
+        let title = tasksList == nil ? "New List" : "Edit List"
+        let message = "Please insert list name"
+        // Название кнопок
+        let doneButtonName = tasksList == nil ? "Save" : "Update"
 
+        // Создание Alert
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        var alertTextField: UITextField!
+        let saveAction = UIAlertAction(title: doneButtonName, style: .default) { _ in
+            
+            guard let newListName = alertTextField.text, !newListName.isEmpty else {
+                return
+            }
+
+            // логика редактирования
+            if let tasksList = tasksList {
+                StorageManager.editList(tasksList, newListName: newListName, complition: complition)
+            // логика создания нового списка
+            } else {
+                let tasksList = TasksList()
+                tasksList.name = newListName
+                StorageManager.saveTasksList(tasksList: tasksList)
+                complition()
+                // Анимированное появление ячейки
+//                self.tableView.insertRows(at: [IndexPath(row: self.tasksLists.count - 1, section: 0)], with: .automatic)
+            }
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+
+        // Настройка Alert
+        alert.addTextField { textField in
+            alertTextField = textField
+            if let listName = tasksList {
+                alertTextField.text = listName.name
+            }
+            alertTextField.placeholder = "List Name"
+        }
+        present(alert, animated: true)
+    }
 }
--- RealmApp/RealmApp/Manager/StorageManager.swift
+++ RealmApp/RealmApp/Manager/StorageManager.swift
@@ -10,7 +10,7 @@ import RealmSwift
 
 let realm = try! Realm()
 
-class StorageManager {
+final class StorageManager {
     
     static func deleteAll() {
         do {
@@ -24,5 +24,42 @@ class StorageManager {
     
     static func getAllTasksLists() -> Results<TasksList> {
         realm.objects(TasksList.self)//.sorted(byKeyPath: "name")
+    }
+    
+    static func saveTasksList(tasksList: TasksList) {
+        do {
+            try realm.write {
+                realm.add(tasksList)
+            }
+        } catch {
+            print("saveTasksList error: \(error)")
+        }
+    }
+
+    static func deleteList(_ tasksList: TasksList) {
+        do {
+            try realm.write {
+                let tasks = tasksList.tasks
+                // последовательно удаляем tasks и tasksList
+                realm.delete(tasks)
+                realm.delete(tasksList)
+            }
+        } catch {
+            print("deleteList error: \(error)")
+        }
+    }
+
+    // Редактирование текущего списка
+    static func editList(_ tasksList: TasksList,
+                         newListName: String,
+                         complition: @escaping () -> Void) {
+        do {
+            try realm.write {
+                tasksList.name = newListName
+                complition()
+            }
+        } catch {
+            print("editList error: \(error)")
+        }
     }
 }
